2a28a47fa36f90ba4df981ef496e661fc94bfb6e
forengeit345 <timophey2907@gmail.com>
2024-02-02 18:38:06 +0500

    Добавлен новый блок - Торшер, небольшие изменения в переводе

--- src/main/java/net/es/endless_store_mod/block/ModBlocks.java
+++ src/main/java/net/es/endless_store_mod/block/ModBlocks.java
@@ -1,5 +1,7 @@
 package net.es.endless_store_mod.block;
 
+import net.es.endless_store_mod.block.custom.LampProp;
+import net.es.endless_store_mod.block.custom.RotateBlock;
 import net.es.endless_store_mod.block.custom.ShelvesProp;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.es.endless_store_mod.EndlessStoreMod;
@@ -15,23 +17,25 @@ import net.minecraft.util.registry.Registry;
 
 public class ModBlocks {
     public static final Block LAMINATE_FLOORING_LIGHT_BLOCK = registerBlock("laminate_flooring_light_block",
-            new Block(AbstractBlock.Settings.of(Material.WOOD).strength(5f).requiresTool()), ModItemGroup.ENDLESS_MOD);
+            new RotateBlock(AbstractBlock.Settings.of(Material.WOOD).strength(5f).requiresTool()), ModItemGroup.ENDLESS_MOD);
     public static final Block LAMINATE_FLOORING_LIGHT_BLOCK_VERTICAL = registerBlock("laminate_flooring_light_block_vertical",
-            new Block(AbstractBlock.Settings.of(Material.WOOD).strength(5f).requiresTool()), ModItemGroup.ENDLESS_MOD);
+            new RotateBlock(AbstractBlock.Settings.of(Material.WOOD).strength(5f).requiresTool()), ModItemGroup.ENDLESS_MOD);
     public static final Block LAMINATE_FLOORING_DARK_BLOCK = registerBlock("laminate_flooring_dark_block",
-            new Block(AbstractBlock.Settings.of(Material.WOOD).strength(5f).requiresTool()), ModItemGroup.ENDLESS_MOD);
+            new RotateBlock(AbstractBlock.Settings.of(Material.WOOD).strength(5f).requiresTool()), ModItemGroup.ENDLESS_MOD);
     public static final Block LAMINATE_FLOORING_MANGROVE_BLOCK = registerBlock("laminate_flooring_mangrove_block",
-            new Block(AbstractBlock.Settings.of(Material.WOOD).strength(5f).requiresTool()), ModItemGroup.ENDLESS_MOD);
+            new RotateBlock(AbstractBlock.Settings.of(Material.WOOD).strength(5f).requiresTool()), ModItemGroup.ENDLESS_MOD);
     public static final Block LAMINATE_FLOORING_PINK_BLOCK = registerBlock("laminate_flooring_pink_block",
-            new Block(AbstractBlock.Settings.of(Material.STONE).strength(5f).requiresTool()), ModItemGroup.ENDLESS_MOD);
+            new RotateBlock(AbstractBlock.Settings.of(Material.STONE).strength(5f).requiresTool()), ModItemGroup.ENDLESS_MOD);
     public static final Block FLOOR_TILES_MULTI_COLORED_BLOCK = registerBlock("floor_tiles_multi_colored_block",
-            new Block(AbstractBlock.Settings.of(Material.STONE).strength(4f).requiresTool()), ModItemGroup.ENDLESS_MOD);
+            new RotateBlock(AbstractBlock.Settings.of(Material.STONE).strength(4f).requiresTool()), ModItemGroup.ENDLESS_MOD);
     public static final Block CONCREATE = registerBlock("concreate_block",
-            new Block(AbstractBlock.Settings.of(Material.STONE).strength(4f).requiresTool()), ModItemGroup.ENDLESS_MOD);
+            new RotateBlock(AbstractBlock.Settings.of(Material.STONE).strength(4f).requiresTool()), ModItemGroup.ENDLESS_MOD);
     public static final Block YELLOW_CONCREATE = registerBlock("yellow_concreate_block",
-            new Block(AbstractBlock.Settings.of(Material.STONE).strength(4f).requiresTool()), ModItemGroup.ENDLESS_MOD);
+            new RotateBlock(AbstractBlock.Settings.of(Material.STONE).strength(4f).requiresTool()), ModItemGroup.ENDLESS_MOD);
     public static final Block SHELVES = registerBlock("shelves_block",
             new ShelvesProp(AbstractBlock.Settings.of(Material.WOOD).strength(2f).requiresTool()), ModItemGroup.ENDLESS_MOD);
+    public static final Block LAMP = registerBlock("lamp",
+            new LampProp(AbstractBlock.Settings.of(Material.WOOD).strength(5f).requiresTool().nonOpaque()), ModItemGroup.ENDLESS_MOD);
     
 
     private static Block registerBlock(String name, Block block, ItemGroup tab) {
